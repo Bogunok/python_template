2b379d8fc33da876e4fc8f94b310c799b1a0e942
Bogunok <elizaveta.bogun04@gmail.com>
2024-03-12 21:24:03 +0200

    Added docstrings to functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,12 +1,50 @@
 
 
 def input_from_console():
+    """
+    Function reads a text from console
+
+    Args:
+        None
+
+    Returns:
+        str: a message to type a string
+
+    Raises:
+        IOError: If user enters incorrect file name or location
+    """
     pass
 
 
 def read_file(file_path):
+    """
+    Function reads a file with a help of built-in functions in Python
+
+    Args:
+        file_path(str): The path of the file user wants to read
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If user enters incorrect file name or location
+        FileNotFoundError: If user wants to access a file that doesn't exist
+    """
     pass
 
 
 def read_file_with_pandas(file_path):
+    """
+    Function reads file with a help of a pandas lib
+
+    Args:
+        file_path(str): The path of the file user wants to read
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If user enters incorrect file name or location
+        FileNotFoundError: If user wants to access a file that doesn't exist
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,33 @@
 
 
-def output_text_to_the_console():
+def output_text_to_the_console(text):
+    """
+    Function displays the text in a console
+
+    Args:
+        text(str): The text that user wants to output in console
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If user entered invalid data
+    """
     pass
 
 
-def write_information_to_file():
+def write_information_to_file(text, file_path):
+    """
+        Function displays the text in a console
+
+        Args:
+            text(str): The text that has to written down in the file
+            file_path(str): The path of the file in which the text will be written
+
+        Returns:
+            str: success message
+
+        Raises:
+            IOError: If user entered invalid data
+        """
     pass
